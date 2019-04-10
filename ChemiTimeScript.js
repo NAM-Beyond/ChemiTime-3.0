c0db38cfe98d5b0f7f9366e62219746644036b51
NAM-Beyond <n.bonnet@hotmail.fr>
2019-04-11 01:04:08 +0200

    - Modified functions order - Added a loop to erase all f****** empty text nodes - Deleted useless LocationCorrection (using ternary operator to avoid modifying the variale - Modified adding Time in order to avoid reconstructing the whole TimeBox. Only the boxes shifted to the right are modified - Lot of simplifications

--- ChemiTimeScript.js
+++ ChemiTimeScript.js
@@ -19,63 +19,60 @@ function AddInputField(ButtonLocation) {
     NewField.setAttribute("onkeypress", "KeyPressed(event, " + ButtonLocation + ")");
     document.getElementById(Grids[CurrentSection - 1]).appendChild(NewField);
 }
+function AddData(InputLocation) {
+    NewValue = document.getElementById(InputLocation).value;
+    MainList[CurrentSection - 1].push([InputLocation, NewValue]);
+    document.getElementById(InputLocation).outerHTML = "";
+    for (x = 0; x < document.getElementById(Grids[CurrentSection - 1]).childNodes.length; x++) {
+        if (document.getElementById(Grids[CurrentSection - 1]).childNodes[x].nodeName == "#text") {
+            document.getElementById(Grids[CurrentSection - 1]).removeChild(document.getElementById(Grids[CurrentSection - 1]).childNodes[x]);
+        }
+    }
+    if (CurrentSection == 1) {
+        AddLine(1, (CurrentRow < 10 ? "0" + CurrentRow : CurrentRow), "01", NewValue);
+        AddButton(1, (CurrentRow + 1 < 10 ? "0" + (CurrentRow + 1) : (CurrentRow + 1)), "01");
+    }
+    if (CurrentSection == 3) {
+        MainList[2].sort();
+        AddButton(3, "01", (CurrentColumn < 10 ? "0" + CurrentColumn : CurrentColumn));
+        for (x = CurrentColumn + 1; x <= document.getElementById("TimeBox").childNodes.length; x++) {
+            if (document.getElementById(3 + "01" + (x < 10 ? "0" + x : x)).localName == "div") {
+                MainList[2][x / 2][0] = parseInt(3 + "01" + (x + 2 < 10 ? "0" + (x + 2) : x + 2));
+            }
+            if (document.getElementById(3 + "01" + (x < 10 ? "0" + x : x)).localName == "button") {
+                document.getElementById(3 + "01" + (x < 10 ? "0" + x : x)).setAttribute("onclick", "AddInputField(" + 3 + "01" + (x + 2 < 10 ? "0" + (x + 2) : x + 2) + ")");
+                document.getElementById(3 + "01" + (x < 10 ? "0" + x : x)).setAttribute("ontouchstart", "AddInputField(" + 3 + "01" + (x + 2 < 10 ? "0" + (x + 2) : x + 2) + ")");
+                document.getElementById(3 + "01" + (x < 10 ? "0" + x : x)).setAttribute("onmouseover", "Light(" + 3 + "01" + (x + 2 < 10 ? "0" + (x + 2) : x + 2) + ", '#FFFFFF', '#0099FF')");
+                document.getElementById(3 + "01" + (x < 10 ? "0" + x : x)).setAttribute("onmouseout", "Light(" + 3 + "01" + (x + 2 < 10 ? "0" + (x + 2) : x + 2) + ", '#00000040', '#FFFFFF')");
+            }
+            document.getElementById(3 + "01" + (x < 10 ? "0" + x : x)).style.gridColumn = x + 2;
+            document.getElementById(3 + "01" + (x < 10 ? "0" + x : x)).id = 3 + "01" + (x + 2 < 10 ? "0" + (x + 2) : x + 2);
+        }
+        AddLine(3, "01", (CurrentColumn + 1 < 10 ? "0"  + (CurrentColumn + 1) : CurrentColumn + 1), NewValue);
+        AddButton(3, "01", (CurrentColumn + 2 < 10 ? "0" + (CurrentColumn + 2) : CurrentColumn + 2));
+    }
+}
+function AddLine(Section, Row, Column, Value) {
+    NewLine = document.createElement("div");
+    NewLine.id = Section.toString() + Row.toString() + Column.toString();
+    NewLine.setAttribute("style", "grid-row: " + Row + "; grid-column: " + Column + ";");
+    NewLine.innerHTML = Value;
+    document.getElementById(Grids[Section - 1]).appendChild(NewLine);
+}
+function AddButton(Section, Row, Column) {
+    NewButton = document.createElement("button");
+    NewButton.id = Section.toString() + Row.toString() + Column.toString();
+    NewButton.setAttribute("style", " grid-row: " + Row + "; grid-column: " + Column + ";");
+    NewButton.setAttribute("onclick", "AddInputField(" + NewButton.id + ")");
+    NewButton.setAttribute("ontouchstart", "AddInputField(" + NewButton.id + ")");
+    NewButton.setAttribute("onmouseover", "Light(" + NewButton.id + ", '#FFFFFF', '#0099FF')");
+    NewButton.setAttribute("onmouseout", "Light(" + NewButton.id + ", '#00000040', '#FFFFFF')");
+    NewButton.innerHTML = "+";
+    document.getElementById(Grids[Section - 1]).appendChild(NewButton);
+}
 function KeyPressed(event, InputLocation) {
     y = event.which || event.keyCode;
     if (y == 13) {
         AddData(InputLocation);
     }
-}
-function AddData(InputLocation) {
-    NewValue = document.getElementById(InputLocation).value;
-    MainList[CurrentSection - 1].push([InputLocation, NewValue]);
-    if (CurrentSection == 1) {
-        document.getElementById(InputLocation).outerHTML = "";
-        LocationCorrection();
-        AddLine(1, CurrentRow, CurrentColumn, NewValue);
-        CurrentRow = parseInt(CurrentRow) + 1;
-        LocationCorrection();
-        AddButton(1, CurrentRow, CurrentColumn);
-    }
-    if (CurrentSection == 3) {
-        MainList[2].sort();
-        document.getElementById("TimeBox").innerHTML = "";
-        AddButton(3, "01", "01");
-        CurrentColumn = 1;
-        for (x = 0; x < MainList[2].length; x++) {
-            CurrentColumn = parseInt(CurrentColumn) + 1;
-            LocationCorrection();
-            AddLine(3, CurrentRow, CurrentColumn, MainList[2][x][1]);
-            CurrentColumn = parseInt(CurrentColumn) + 1;
-            LocationCorrection();
-            AddButton(3, CurrentRow, CurrentColumn);
-        }
-    }
-}
-function LocationCorrection() {
-    if (parseInt(CurrentRow) < 10) {
-        CurrentRow = "0" + parseInt(CurrentRow);
-    }
-    if (parseInt(CurrentColumn) < 10) {
-        CurrentColumn = "0" + parseInt(CurrentColumn);
-    }
-}
-function AddLine(Section, Row, Column, Value) {
-    NewData = document.createElement("div");
-    NewData.innerHTML = Value;
-    LineLocation = Section.toString() + Row.toString() + Column.toString();
-    NewData.id = LineLocation;
-    NewData.setAttribute("style", "grid-row: " + Row + "; grid-column: " + Column + ";");
-    document.getElementById(Grids[Section - 1]).appendChild(NewData);
-}
-function AddButton(Section, Row, Column) {
-    NewButton = document.createElement("button");
-    ButtonId = Section.toString() + Row.toString() + Column.toString();
-    NewButton.id = ButtonId;
-    NewButton.setAttribute("style", " grid-row: " + Row + "; grid-column: " + Column + ";");
-    NewButton.setAttribute("onclick", "AddInputField(" + ButtonId + ")");
-    NewButton.setAttribute("ontouchstart", "AddInputField(" + ButtonId + ")");
-    NewButton.setAttribute("onmouseover", "Light(" + ButtonId + ", '#FFFFFF', '#0099FF')");
-    NewButton.setAttribute("onmouseout", "Light(" + ButtonId + ", '#00000040', '#FFFFFF')");
-    NewButton.innerHTML = "+";
-    document.getElementById(Grids[Section - 1]).appendChild(NewButton);
 }
