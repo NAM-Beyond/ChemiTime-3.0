de6da04c43db4eede2f39afc2bd65240bf0d7242
NAM-Beyond <n.bonnet@hotmail.fr>
2019-05-17 19:18:49 +0200

    Huge rework of the code : - Now objects are not counted by ColumnStart but by ItemNumber in order to facilitate the ColumnCorrection function (no "null" object while looping over the column number) - Almost everything done to make ColumnCorrection of the DoseBox possible when a new date is entered - Seems to have only one bug left : when a dose is more than 3 columns long, it is isn't extended further when it should be

--- ChemiTimeScript.js
+++ ChemiTimeScript.js
@@ -2,69 +2,76 @@ var MainList = [[],[],[]];
 var CurrentSection = 0;
 var CurrentRow = 0;
 var CurrentColumn = 0;
+var ItemNumber = 0;
 var Grids = ["INNBox", "DoseBox", "TimeBox"];
-function Light(ButtonLocation, ButtonFontColor, ButtonBackgroundColor) {
-    document.getElementById(ButtonLocation).style.color = ButtonFontColor;
-    document.getElementById(ButtonLocation).style.backgroundColor = ButtonBackgroundColor;
-}
-function AddInputField(ButtonLocation) {
-    CurrentSection = Math.floor(ButtonLocation / 10000);
-    CurrentRow = Math.floor((ButtonLocation - CurrentSection * 10000) / 100);
-    CurrentColumn = ButtonLocation - CurrentSection * 10000 - CurrentRow * 100;
-    document.getElementById(ButtonLocation).outerHTML = "";
-    NewField = document.createElement("input");
-    NewField.type = "text";
-    NewField.id = ButtonLocation;
-    NewField.setAttribute("style", "grid-row: " + CurrentRow + "; grid-column: " + CurrentColumn + "/" + (CurrentSection == 2 ? CurrentColumn + 3 : CurrentColumn) + ";");
-    NewField.setAttribute("onkeypress", "KeyPressed(event, " + ButtonLocation + ")");
-    document.getElementById(Grids[CurrentSection - 1]).appendChild(NewField);
-}
-function AddData(InputLocation) {
-    NewValue = document.getElementById(InputLocation).value;
-    MainList[CurrentSection - 1].push([InputLocation, NewValue]);
-    document.getElementById(InputLocation).outerHTML = "";
-    for (x = 0; x < document.getElementById(Grids[CurrentSection - 1]).childNodes.length; x++) {
-        if (document.getElementById(Grids[CurrentSection - 1]).childNodes[x].nodeName == "#text") {
-            document.getElementById(Grids[CurrentSection - 1]).removeChild(document.getElementById(Grids[CurrentSection - 1]).childNodes[x]);
+window.onload = function RemoveFuckingUselessEmptyTextNodes() {
+    for (x = 0; x < 3; x ++) {
+        for (y = 0; y < document.getElementById(Grids[x]).childNodes.length; y++) {
+            if (document.getElementById(Grids[x]).childNodes[y].nodeName == "#text") {
+                document.getElementById(Grids[x]).removeChild(document.getElementById(Grids[x]).childNodes[y]);
+            }
         }
     }
+}
+function Light(ButtonId, ButtonFontColor, ButtonBackgroundColor) {
+    document.getElementById(ButtonId).style.color = ButtonFontColor;
+    document.getElementById(ButtonId).style.backgroundColor = ButtonBackgroundColor;
+}
+function AddInputField(ButtonId) {
+    CurrentSection = Math.floor(ButtonId / 10000);
+    CurrentRow = parseInt(document.getElementById(ButtonId).style.gridRowStart);
+    CurrentColumn = parseInt(document.getElementById(ButtonId).style.gridColumnStart);
+    ColumnEnd = parseInt(document.getElementById(ButtonId).style.gridColumnEnd);
+    ItemNumber = ButtonId - CurrentSection * 10000 - CurrentRow * 100;
+    document.getElementById(ButtonId).outerHTML = "";
+    NewField = document.createElement("input");
+    NewField.type = "text";
+    NewField.id = ButtonId;
+    NewField.setAttribute("style", "grid-row: " + CurrentRow + "; grid-column: " + CurrentColumn + " / " + ColumnEnd + ";");
+    NewField.setAttribute("onkeypress", "KeyPressed(event, " + ButtonId + ")");
+    document.getElementById(Grids[CurrentSection - 1]).appendChild(NewField);
+}
+function AddData(InputId) {
+    NewValue = document.getElementById(InputId).value;
+    MainList[CurrentSection - 1].push([InputId, NewValue]);
+    document.getElementById(InputId).outerHTML = "";
     if (CurrentSection == 1) {
-        AddLine(1, CurrentRow, 1, 1, NewValue);
-        AddButton(1, CurrentRow + 1, 1, 1);
-        AddLine(2, CurrentRow, 1, 3, "&nbsp;");
-        AddLine(2, CurrentRow, (document.getElementById("TimeBox").childNodes.length + MainList[2].length - 1), (document.getElementById("TimeBox").childNodes.length + MainList[2].length + 1), "&nbsp;");
+        AddItem(1, CurrentRow, 1, 1, "div", NewValue, 1);
+        AddItem(1, CurrentRow + 1, 1, 1, "button", "+", 1);
+        if (MainList[2].length > 0) {
+            AddItem(2, CurrentRow, 1, 3, "div", "&nbsp;", 1);
+            AddItem(2, CurrentRow, (document.getElementById("TimeBox").childNodes.length + MainList[2].length - 1), (document.getElementById("TimeBox").childNodes.length + MainList[2].length + 1), "div", "&nbsp;", MainList[2].length + 1);
+        }
         for (x = 0; x < MainList[2].length - 1; x++) {
-            AddButton(2, CurrentRow, 3 * (x + 1), 3 * (x + 2));
+            AddItem(2, CurrentRow, 3 * (x + 1), 3 * (x + 2), "button", "+", x + 2);
         }
     }
     if (CurrentSection == 3) {
         MainList[2].sort();
-        AddButton(3, 1, CurrentColumn, CurrentColumn);
-        GridCorrection(3, "01", CurrentColumn + 1);
-        AddLine(3, 1, CurrentColumn + 1, CurrentColumn + 3, NewValue);
-        AddButton(3, 1, CurrentColumn + 3, CurrentColumn + 3);
+        AddItem(3, 1, CurrentColumn, CurrentColumn, "button", "+", ItemNumber);
+        if (ItemNumber + 1 < document.getElementById("TimeBox").childNodes.length) {
+            ColumnCorrection(3, 1, document.getElementById("TimeBox").childNodes.length, ItemNumber + 1, 2);
+        }
+        AddItem(3, 1, CurrentColumn + 1, CurrentColumn + 3, "div", NewValue, ItemNumber + 1);
+        AddItem(3, 1, CurrentColumn + 3, CurrentColumn + 3, "button", "+", ItemNumber + 2);
+        for (y = 1; y <= MainList[0].length; y++) {
+            ColumnCorrection(2, y, MainList[2].length, Math.floor(ItemNumber / 2) + 1, 1);
+        }
     }
     if (CurrentSection == 2) {
-        AddLine(2, CurrentRow, CurrentColumn, CurrentColumn + 3, NewValue);
+        AddItem(2, CurrentRow, CurrentColumn, CurrentColumn + 3, "div", NewValue, ItemNumber);
     }
 }
-function AddLine(Section, Row, DebutColumn, EndColumn, Value) {
-    NewLine = document.createElement("div");
-    NewLine.id = Section + (Row < 10 ? "0" + Row : Row) + (DebutColumn < 10 ? "0" + DebutColumn : DebutColumn);
-    NewLine.setAttribute("style", "grid-row: " + Row + "; grid-column: " + DebutColumn + "/" + EndColumn + ";");
-    NewLine.innerHTML = Value;
-    document.getElementById(Grids[Section - 1]).appendChild(NewLine);
-}
-function AddButton(Section, Row, DebutColumn, EndColumn) {
-    NewButton = document.createElement("button");
-    NewButton.id = Section + (Row < 10 ? "0" + Row : Row) + (DebutColumn < 10 ? "0" + DebutColumn : DebutColumn);
-    NewButton.setAttribute("style", "grid-row:" + Row + "; grid-column:" + DebutColumn + "/" + EndColumn + ";");
-    NewButton.setAttribute("onclick", "AddInputField(" + NewButton.id + ")");
-    NewButton.setAttribute("ontouchstart", "AddInputField(" + NewButton.id + ")");
-    NewButton.setAttribute("onmouseover", "Light(" + NewButton.id + ", '#FFFFFF', '#0099FF')");
-    NewButton.setAttribute("onmouseout", "Light(" + NewButton.id + ", '#00000040', '#FFFFFF')");
-    NewButton.innerHTML = "+";
-    document.getElementById(Grids[Section - 1]).appendChild(NewButton);
+function AddItem(Section, Row, DebutColumn, EndColumn, Type, Value, ItemNumber) {
+    NewItem = document.createElement(Type);
+    document.getElementById(Grids[Section - 1]).appendChild(NewItem);
+    NewItem.id = Section + (Row < 10 ? "0" + Row : Row) + (ItemNumber < 10 ? "0" + ItemNumber : ItemNumber);
+    if (Type == "div") {
+        NewItem.outerHTML = "<div id=\"" + NewItem.id + "\" style=\"grid-row: " + Row + "; grid-column: " + DebutColumn + " / " + EndColumn + ";\">" + Value + "</div>";
+    }
+    else {
+        NewItem.outerHTML = "<button id=\"" + NewItem.id + "\" style=\"grid-row: " + Row + "; grid-column: " + DebutColumn + " / " + EndColumn + ";\" onclick=\"AddInputField(" + NewItem.id + ")\" ontouchstart=\"AddInputField(" + NewItem.id + ")\" onmouseover=\"Light('" + NewItem.id + "', '#FFFFFF', '#0099FF')\" onmouseout=\"Light('" + NewItem.id + "', '#00000040', '#FFFFFF40')\">" + Value + "</button>";
+    }
 }
 function KeyPressed(event, InputLocation) {
     y = event.which || event.keyCode;
@@ -72,24 +79,36 @@ function KeyPressed(event, InputLocation) {
         AddData(InputLocation);
     }
 }
-function GridCorrection(Section, Row, Column) {
-    for (x = document.getElementById(Grids[Section - 1]).childNodes.length + MainList[Section - 1].length; x >= Column; x--) {
-        if (document.getElementById(Section + Row + (x < 10 ? "0" + x : x)) == null) {
-            continue;
+function ColumnCorrection(Section, Row, ChangeStart, ChangeEnd, ColumnShift) {
+    if (Section == 2 && document.getElementById(Section + (Row < 10 ? "0" + Row : Row) + (ChangeEnd < 10 ? "0" + ChangeEnd : ChangeEnd)).localName == "div" && document.getElementById(Section + (Row < 10 ? "0" + Row : Row) + (ChangeEnd < 10 ? "0" + ChangeEnd : ChangeEnd)).innerHTML != "&nbsp;") {
+        ColumnShift = 0;
+    }
+    for (x = ChangeStart; x >= ChangeEnd; x--) {
+        TempElement = document.getElementById(Section + (Row < 10 ? "0" + Row : Row) + (x < 10 ? "0" + x : x));
+        if (Section == 2 && x == ChangeEnd && ChangeEnd != ChangeStart) {
+            if (x == 1 || TempElement.localName == "button") {
+                AddItem(2, Row, parseInt(TempElement.style.gridColumnEnd), parseInt(TempElement.style.gridColumnEnd) + 3, "button", "+", x + 1);
+            }
+            else if (TempElement.localName == "div" && TempElement.innerHTML != "&nbsp;") {
+                console.log("extension de la dose : ", TempElement.id);
+                TempElement.style.gridColumnEnd = parseInt(TempElement.style.gridColumnEnd) + 3;
+            }
         }
-        else if (document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).localName == "div" && document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).innerHTML != "&nbsp;") {
-            MainList[Section - 1][(x - 2) / 3][0] = parseInt(Section + Row + (x + 3 < 10 ? "0" + (x + 3) : x + 3));
-            document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).setAttribute("style", "grid-row:" + Row + "; grid-column:" + (x + 3) + "/" + (x + 5) + ";");
-            document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).id = Section + Row + (x + 3 < 10 ? "0" + (x + 3) : x + 3);
-            continue;
-        }
-        else if (document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).localName == "button") {
-            document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).setAttribute("onclick", "AddInputField(" + Section + Row + (x + 3 < 10 ? "0" + (x + 3) : x + 3) + ")");
-            document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).setAttribute("ontouchstart", "AddInputField(" + Section + Row + (x + 3 < 10 ? "0" + (x + 3) : x + 3) + ")");
-            document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).setAttribute("onmouseover", "Light(" + Section + Row + (x + 3 < 10 ? "0" + (x + 3) : x + 3) + ", '#FFFFFF', '#0099FF')");
-            document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).setAttribute("onmouseout", "Light(" + Section + Row + (x + 3 < 10 ? "0" + (x + 3) : x + 3) + ", '#00000040', '#FFFFFF')");
-            document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).setAttribute("style", "grid-row:" + Row + "; grid-column:" + (x + 3) + "/" + (x + 3) + ";");
-            document.getElementById(Section + Row + (x < 10 ? "0" + x : x)).id = Section + Row + (x + 3 < 10 ? "0" + (x + 3) : x + 3);
+        else {
+            TempElement.style.gridColumnStart = parseInt(TempElement.style.gridColumnStart) + 3;
+            TempElement.style.gridColumnEnd = parseInt(TempElement.style.gridColumnEnd) + 3;
+            OldId = TempElement.id.toString();
+            NewId = Section + (Row < 10 ? "0" + Row : Row) + (x + ColumnShift < 10 ? "0" + (x + ColumnShift) : x + ColumnShift);
+            TempElement.outerHTML = TempElement.outerHTML.replace(new RegExp(OldId, 'g'), NewId);
+            if (x == ChangeEnd && TempElement.innerHTML == "&nbsp;") {
+                AddItem(2, Row, parseInt(TempElement.style.gridColumnStart) - 3, parseInt(TempElement.style.gridColumnStart), "button", "+", x);
+            }
+            if (Section == 2 && TempElement.localName == "div" && TempElement.innerHTML != "&nbsp;") {
+                /*MainList[1][x][0] = parseInt(NewId);*/
+            }
+            if (Section == 3 && TempElement.localName == "div") {
+                /*MainList[2][x][0] = parseInt(NewId);*/
+            }
         }
     }
 }
